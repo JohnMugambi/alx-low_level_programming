c7eb6e21991cba11b00656bde2fe22dabaa0fcc3
JohnMugambi <mugambijm@ymail.com>
2022-12-14 12:53:53 -0800

    Adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two nubs
+ *
+ * @num1: first numb
+ *
+ * @num2: second numb
+ *
+ * Return:result
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
