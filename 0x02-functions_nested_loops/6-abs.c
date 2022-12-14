e0f527cc01770fa43c4b4e12158e75cfce2d0391
JohnMugambi <mugambijm@ymail.com>
2022-12-14 12:28:36 -0800

    commit absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of a number.
+ *
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
