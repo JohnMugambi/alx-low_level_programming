6c46c4415829bead29bef97fb8addb5c1fbf561d
JohnMugambi <mugambijm@ymail.com>
2022-12-14 13:01:45 -0800

    betty error

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,9 @@
 #include "main.h"
-
 /**
- *
- *
  * jack_bauer - Prints every minute of the day of
  *              Jack Bauer, starting from 00:00 to 23:59.
- *
- *
- * Return : always 0
  */
+
 void jack_bauer(void)
 {
 	int hour, minute;
