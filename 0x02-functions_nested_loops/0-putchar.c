8799c2d1066896d9f018cc77b8225652bd23b0a7
JohnMugambi <mugambijm@ymail.com>
2022-12-14 12:01:03 -0800

    Return value of 'main'

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * main -  printss _putchar
+ * main -  prints _putchar
+ * 
+ * Description: this is a putchar program
  *
- * Return : always 0
+ * Return: always 0
  *
  */
 int main(void)
