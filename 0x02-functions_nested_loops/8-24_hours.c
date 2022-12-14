64819dea6baa2b7e4399d815aac5aa1ea048e1da
JohnMugambi <mugambijm@ymail.com>
2022-12-14 12:48:19 -0800

    Jack bauer commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,6 +3,8 @@
 /**
  * jack_bauer : prints every minute from 0000 to 2359
  *
+ * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
  * Description: jack start from 00 to 23.59
  *
  * Return : always 0
