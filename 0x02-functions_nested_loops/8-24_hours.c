5abf9bee7f23ca705e30ed735af6632230c2f170
JohnMugambi <mugambijm@ymail.com>
2022-12-14 12:44:28 -0800

    Jack bauer commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,13 +3,15 @@
 /**
  * jack_bauer : prototype
  *
+ * Description: jack start from 00 to 23.59
+ *
  * Return : always 0
  */
 void jack_bauer(void)
 {
-	int hour,minute;
+	int hour, minute;
 
-	for (hour = 0; hour <=23; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
