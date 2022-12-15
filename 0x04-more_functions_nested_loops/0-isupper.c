bfa7b775ff4325a5373d8e999cd68a351ae9780c
JohnMugambi <mugambijm@ymail.com>
2022-12-15 10:33:22 -0800

    check if is digit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks is a character is uppercase
+ * @c: carrier integer variable
+ * Return: 1 true, 0 false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
