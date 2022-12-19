04e15c9af22176cce96e84d53178a9a5d9b50b3b
JohnMugambi <mugambijm@ymail.com>
2022-12-19 12:29:40 -0800

    Commit files

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,3 +5,7 @@ int _putchar(char c);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
